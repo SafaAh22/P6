f7a4c8afc07ae0fb4e832b9af28bc493f900666e
SafaAh22 <141917231+SafaAhAwa@users.noreply.github.com>
2023-09-29 15:41:47 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,9 @@
 import streamlit as st
 import numpy as np
 import cv2
-from tensorflow.keras.models import load_model
+import tensorflow as tf
+# from tensorflow.keras.models import load_model
 
 
-# Load the model from the directory
-model = load_model("modelh5.h5")
+# # Load the model from the directory
+# model = load_model("modelh5.h5")
