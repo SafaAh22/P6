c51ea03a79fef4b1e971cbd795bf1e3e8e39356f
SafaAh22 <141917231+SafaAhAwa@users.noreply.github.com>
2023-09-29 16:11:52 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,16 @@ import sklearn
 import tensorflow
 import tensorflow_hub as hub
 from tensorflow.keras.models import load_model
+import numpy as np
+from tensorflow.keras.preprocessing import image
+from io import StringIO
 
 st.write('Projet 6')
 
 # Load the model from the directory
 model = load_model("modelh5")
+
+
+with st.form("my_form"):
+   st.write("Upload Dogs Picture")
+   uploaded_file = st.file_uploader("Choose a file")
