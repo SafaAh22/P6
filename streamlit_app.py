f8e0e25904e830cd1d001d98cf8f22b9e9a30f1e
SafaAh22 <aikimura2013@gmail.com>
2023-09-29 16:30:24 +0300

    name breeds

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,7 @@ import tensorflow_hub as hub
 from tensorflow.keras.models import load_model
 import numpy as np
 from tensorflow.keras.preprocessing import image
+from PIL import Image
 from io import StringIO
 
 st.write('Projet 6')
@@ -14,12 +15,24 @@ st.write('Projet 6')
 # Load the model from the directory
 model = load_model("modelh5")
 
+# Define a function to load and preprocess image
+def load_and_preprocess_image(uploaded_file):
+    # Load image and set the size to (224, 224)
+    img = Image.open(uploaded_file).resize((224, 224))
+    
+    # Convert image to array and expand dimensions
+    img_array = image.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    
+    # Normalize the image
+    img_array /= 255.
+
 
 with st.form("my_form"):
    st.write("Upload Dogs Picture")
-   uploaded_file = st.file_uploader("Choose a file")
+   uploaded_file = st.file_uploader("Choose an image", type=["jpg", "jpeg", "png"])
 
    # Every form must have a submit button.
    submitted = st.form_submit_button("Submit")
    if submitted:
-      print('submited')
+      if uploaded_file is not None:
