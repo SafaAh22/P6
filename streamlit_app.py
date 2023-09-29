25e0bc8be12fb37a0645953440a60337be30ca7a
SafaAh22 <141917231+SafaAhAwa@users.noreply.github.com>
2023-09-29 15:55:53 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,7 @@
-# importing librarys
 import streamlit as st
+import joblib
 import numpy as np
-# import cv2
-import tensorflow as tf
-# from tensorflow.keras.models import load_model
-
-
-# # Load the model from the directory
-# model = load_model("modelh5.h5")
+import pandas as pd
+import sklearn
+import tensorflow
+import tensorflow_hub as hub
